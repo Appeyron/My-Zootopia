70e6bc2ed0d55303f7cb37d3601fa34e434652ae
Appeyron <jens.george@outlook.de>
2026-05-27 15:24:37 +0200

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,31 +2,65 @@ import json
 
 
 def load_data(file_path):
-    """ Loads a JSON file """
+    """Load a JSON file."""
+
     with open(file_path, "r") as handle:
         return json.load(handle)
 
 
-def print_animals(animals):
-     """ Prints name, diet, first location and type of animals if they exist"""
+def generate_animals_html(data):
+    """Generate HTML for all animals."""
 
-     for animal in animals:
-         if "name" in animal:
-             print(f"Name: {animal['name']}")
+    output = ""
 
-         if "diet" in animal['characteristics']:
-             print(f"Diet: {animal['characteristics']['diet']}")
+    for animal_data in data:
 
-         if "locations" in animal:
-             print(f"Location: {', '.join(animal['locations'])}")
+        if "name" in animal_data:
+            output += f"Name: {animal_data['name']} "
 
-         if "type" in animal['characteristics']:
-             print(f"Type: {animal['characteristics']['type']}")
+        if "diet" in animal_data["characteristics"]:
+            output += (
+                f"Diet: "
+                f"{animal_data['characteristics']['diet']} "
+            )
 
-         print()
+        if "locations" in animal_data:
+            output += (
+                f"Location: "
+                f"{', '.join(animal_data['locations'])} "
+            )
+
+        if "type" in animal_data["characteristics"]:
+            output += (
+                f"Type: "
+                f"{animal_data['characteristics']['type']}"
+            )
+
+        output += "\n</li>\n"
+
+    return output
 
 
+def main():
+    """Generate animals.html from template and JSON data."""
+
+    data = load_data("animals_data.json")
+
+    output = generate_animals_html(data)
+
+    with open("animals_template.html", "r") as handle:
+        html_template = handle.read()
+
+    final_html = html_template.replace(
+        "__REPLACE_ANIMALS_INFO__",
+        output
+    )
+
+    with open("animals.html", "w") as handle:
+        handle.write(final_html)
+
+    print(output)
 
 
-animals_data = load_data('animals_data.json')
-print_animals(animals_data)
+if __name__ == "__main__":
+    main()
